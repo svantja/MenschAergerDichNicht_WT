31e3d58332a4a9f6641421c0d534226093276969
svantja <s.loeger@web.de>
2017-11-29 10:13:16 +0100

    added css file

--- app/controllers/MenschController.scala
+++ app/controllers/MenschController.scala
@@ -5,14 +5,16 @@ import play.api.mvc._
 import de.htwg.se.menschaergerdichnicht.Game
 import de.htwg.se.menschaergerdichnicht.controller.controllerComponent.GameState
 import scala.io.StdIn.readLine
+import de.htwg.se.menschaergerdichnicht.controller.controllerComponent.GameState
 
 class MenschController @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
 
   val gameController = Game.controller
-  def tui = gameController.tui.toHtml
+  def tui = gameController.tui
 
   def mensch = Action {
-    Ok(tui)
+    Ok(views.html.mensch(gameController))
   }
+
 
 }
