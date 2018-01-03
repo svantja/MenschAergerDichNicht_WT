4f5e60fef22f240057b421a5aefdc29df14e49a0
svantja <s.loeger@web.de>
2018-01-03 15:23:45 +0100

    added buttons, href to actions, moving tokens works

--- app/controllers/MenschController.scala
+++ app/controllers/MenschController.scala
@@ -7,12 +7,34 @@ import de.htwg.se.menschaergerdichnicht.controller.controllerComponent.GameState
 import scala.io.StdIn.readLine
 import de.htwg.se.menschaergerdichnicht.controller.controllerComponent.GameState
 
+@Singleton
 class MenschController @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
 
   val gameController = Game.controller
   def tui = gameController.tui
 
   def mensch = Action {
+    Ok(views.html.mensch(gameController))
+  }
+
+  def newPlayer(index: Int) = Action{
+    gameController.addPlayer(index.toString)
+    Ok(views.html.mensch(gameController))
+  }
+
+  def start = Action{
+    gameController.startGame()
+    gameController.gameState == "ONGOING"
+    Ok(views.html.mensch(gameController))
+  }
+
+  def dicing = Action {
+    gameController.startGame()
+    Ok(views.html.mensch(gameController))
+  }
+
+  def move(id: Int) = Action{
+    gameController.chooseToken(id)
     Ok(views.html.mensch(gameController))
   }
 
